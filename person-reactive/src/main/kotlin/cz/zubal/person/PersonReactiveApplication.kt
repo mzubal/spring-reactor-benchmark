ac9081341f1545e278114e13c7dc94f73663def2
James Dunnam <7660553+jimador@users.noreply.github.com>
2019-01-04 11:21:28 -0500

    replacing delay/sleep with bubble sort for simulating work and updating ignore

--- person-reactive/src/main/kotlin/cz/zubal/person/PersonReactiveApplication.kt
+++ person-reactive/src/main/kotlin/cz/zubal/person/PersonReactiveApplication.kt
@@ -2,6 +2,7 @@ package cz.zubal.person
 
 import io.codearte.jfairy.Fairy
 import io.codearte.jfairy.producer.person.Person
+import io.codearte.jfairy.producer.person.PersonProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
-import java.time.Duration
+import java.util.concurrent.ThreadLocalRandom
 
 @SpringBootApplication
 class PersonReactiveApplication
@@ -37,12 +38,47 @@ class PersonController {
 
     private fun mono() : Mono<Person> {
         return Mono
-                .fromCallable(this::generatePerson)
-                .delayElement(Duration.ofMillis(1000))
+                .fromCallable(this::getPerson)
                 .subscribeOn(scheduler)
     }
 
+    /**
+     * Simulate doing work by bubble sorting a random list of people and getting one from that list
+     */
+    private fun getPerson(): Person {
+        val persons = mutableListOf<Person>()
+        // add a random number of people to the list
+        for (i in 0..ThreadLocalRandom.current().nextInt(10, 1000)) {
+            persons.add(generatePerson())
+        }
+        // bubble sort the list by person age and return the first one
+        return bubbleSortPeopleByAge(persons)[0]
+    }
+
+    /**
+     * generate a person with an age between 0 and 9999
+     */
     private fun generatePerson() : Person {
-        return fairy.person()
+        return fairy.person(PersonProperties.ageBetween(0, 9999))
+    }
+
+    /**
+     * dumb n^2 bubble sort
+     */
+    private fun bubbleSortPeopleByAge(people: MutableList<Person>): List<Person> {
+        val n = people.size
+        var temp: Person
+
+        for (i in 0 until n) {
+            for (j in 1 until n - i) {
+                if (people[j - 1].age > people[j].age) {
+                    temp = people[j - 1]
+                    people[j - 1] = people[j]
+                    people[j] = temp
+                }
+            }
+        }
+
+        return people
     }
 }
--- person-servlet/src/main/kotlin/cz/zubal/person/PesonServletApplication.kt
+++ person-servlet/src/main/kotlin/cz/zubal/person/PesonServletApplication.kt
@@ -2,12 +2,14 @@ package cz.zubal.person
 
 import io.codearte.jfairy.Fairy
 import io.codearte.jfairy.producer.person.Person
+import io.codearte.jfairy.producer.person.PersonProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ThreadLocalRandom
 
 @SpringBootApplication
 class PersonReactiveApplication
@@ -24,12 +26,46 @@ class PersonController {
 
     @GetMapping
     fun persons(@RequestParam number : Long) : List<Person> {
-        return (1..number).map { generatePerson() }
+        return (1..number).map { getPerson() }
     }
 
+    /**
+     * Simulate doing work by bubble sorting a random list of people and getting one from that list
+     */
+    private fun getPerson(): Person {
+        val persons = mutableListOf<Person>()
+        for (i in 0..ThreadLocalRandom.current().nextInt(10, 1000)) {
+            persons.add(generatePerson())
+        }
+
+        return bubbleSortPeopleByAge(persons)[0]
+    }
+
+    /**
+     * generate a person with an age between 0 and 9999
+     */
     private fun generatePerson() : Person {
-        Thread.sleep(1000)
-        return fairy.person()
+        return fairy.person(PersonProperties.ageBetween(0, 9999))
+    }
+
+    /**
+     * dumb n^2 bubble sort
+     */
+    private fun bubbleSortPeopleByAge(people: MutableList<Person>): List<Person> {
+        val n = people.size
+        var temp: Person
+
+        for (i in 0 until n) {
+            for (j in 1 until n - i) {
+                if (people[j - 1].age > people[j].age) {
+                    temp = people[j - 1]
+                    people[j - 1] = people[j]
+                    people[j] = temp
+                }
+            }
+        }
+
+        return people
     }
 
 }
